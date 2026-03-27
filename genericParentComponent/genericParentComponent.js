1bb6ff45b44b91bf330658f139d625597e02d483
Swagata Dasgupta <sdg.githubofficial@gmail.com>
2026-03-27 15:54:49 +0530

    Update genericParentComponent.js

--- genericParentComponent/genericParentComponent.js
+++ genericParentComponent/genericParentComponent.js
@@ -23,6 +23,7 @@ export default class GenericParentComponent extends LightningElement {
         this._stepIndex = value;
         for (const [index, refKey] of this.stepRefs.entries()) {
             const ref = this.refs[refKey];
+            console.log(JSON.stringify(ref))
             if (!ref) {
                 continue;
             }
@@ -36,6 +37,20 @@ export default class GenericParentComponent extends LightningElement {
 
     get currentStepLabel(){        
         return this.stepLabels[this.stepIndex];
+    }
+
+    get showPrevious(){
+        return this.stepIndex > 0;
+    }
+    get showNext(){
+        return this.stepIndex < this.stepLabels.length - 1;
+    }
+
+    handlePrevious(){
+        this.stepIndex--;
+    }
+    handleNext(){
+        this.stepIndex++;
     }
 
     loremIpsum1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt, nisi et tincidunt feugiat, lacus massa vestibulum libero, eu dignissim orci dui et sapien. Nam finibus tempus eros, at commodo nulla posuere quis. Morbi scelerisque pellentesque iaculis. Donec nulla nunc, vestibulum id sapien suscipit, aliquam pellentesque libero. Mauris luctus sagittis velit, accumsan aliquet felis egestas vel. Pellentesque blandit eros dui, ac viverra odio vestibulum at. Ut convallis, est eu tincidunt dignissim, nunc tellus venenatis tellus, quis laoreet felis arcu in nibh. Proin non ipsum sed dui accumsan finibus eleifend nec risus. Curabitur elementum varius hendrerit. Etiam varius mollis rutrum. Sed fringilla eros non placerat condimentum. Mauris faucibus interdum nunc, sed imperdiet lectus tincidunt et.';
